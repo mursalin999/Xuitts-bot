fec560ed70820f2097390fe0d25d6258573fb1e3
Ibrahim Mursalin <155314074+mursalin999@users.noreply.github.com>
2024-01-25 23:43:00 +0600

    Create Node.js
    Signed-off-by: Ibrahim Mursalin  <155314074+mursalin999@users.noreply.github.com>

---
+++ Node.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// Read configuration from config.dev.json
+const configDev = JSON.parse(fs.readFileSync('config.dev.json', 'utf-8'));
+
+// ... Rest of the code remains unchanged
